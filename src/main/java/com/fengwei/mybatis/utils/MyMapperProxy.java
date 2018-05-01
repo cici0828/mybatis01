f9ff64d1e1a0b2435989e408c96f62da8df7e1a4
cici0828 <dde20011@hotmail.com>
2018-05-01 18:22:22 +0800

    基本学完了myBatis XML配置

---
+++ src/main/java/com/fengwei/mybatis/utils/MyMapperProxy.java
@@ -0,0 +1,24 @@
+package com.fengwei.mybatis.utils;
+
+import org.apache.ibatis.session.SqlSession;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class MyMapperProxy<T> implements InvocationHandler
+{
+    private Class<T>  mapperInterface;
+    private SqlSession sqlSession;
+
+    public MyMapperProxy(Class<T> mapperInterface, SqlSession sqlSession){
+        this.mapperInterface = mapperInterface;
+        this.sqlSession = sqlSession;
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        List<T> list = sqlSession.selectList(mapperInterface.getCanonicalName() +  "." +
+        method.getName());
+        return list;
+    }
+}
